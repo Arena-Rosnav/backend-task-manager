24a634f1a8c2e57e90febd5769433d265a4363cb
ReykCS <reyk-carstens@web.de>
2023-05-26 14:12:29 +0200

    changed entry.sh to contain env vars

--- docker/plotting/entry.sh
+++ docker/plotting/entry.sh
@@ -4,5 +4,12 @@ source ./devel/setup.sh
 # mv /root/startup/plotting_data.yaml /root/arena-evaluation/plot_declarations/plotting_data.yaml
 # Env variablen an arena-evaluation übergeben?
 # mv "/root/startup/.env" "/root/arena-evaluation/.env"
-python3 /root/src/arena-evaluation/create_plots.py plotting_data.yaml
+
+export TASK_ID=$1
+export APP_TOKEN_KEY=$2
+export APP_TOKEN=$3
+export API_BASE_URL=$4
+export FINISH_TASK_ENDPOINT=$5
+
+python3 /root/src/arena-evaluation/create_plots.py plotting_data.yaml --is_webapp_docker true
 # Send plots to the server?
